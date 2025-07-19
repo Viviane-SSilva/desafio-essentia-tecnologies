528df8868e6b27eab1da3a5fcd87bb4d68409cca
Viviane Silva <viviane.silva1810@gmail.com>
2025-07-19 16:36:33 -0300

    chore: ajuste de importacao

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import taskRoutes from './routes/task.routes'
+import { routes } from './routes/task.routes'
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+app.use(routes)
 
-app.use('/tasks', taskRoutes)
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000')
