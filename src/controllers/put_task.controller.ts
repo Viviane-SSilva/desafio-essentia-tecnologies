34cfa918d2ffe06aee05f151130182de2459af33
Viviane Silva <viviane.silva1810@gmail.com>
2025-07-20 11:11:19 -0300

    chore: criacao da rota update

---
+++ src/controllers/put_task.controller.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/cliente";
+import { Request, Response } from "express"
+
+
+async function handleUpdateTask(request: Request, response: Response) {
+
+    const { id } = request.params;
+    const { title } = request.body;
+
+
+    const updateTask = await prisma.task.update({
+        where: { id: Number(id) },
+        data: {
+            title,
+            updateAt: new Date(),
+        },
+    })
+
+     return response.send(updateTask)
+}
+
+
+export { handleUpdateTask }
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { handleCreateTask } from "@/controllers/create_task.controller";
 import { handleGetAllTask } from "@/controllers/get_task.controller";
+import { handleDeleteTask } from "@/controllers/delete_task.controller";
+import { handleUpdateTask } from "@/controllers/put_task.controller";
 
 const routes = Router()
 //
 routes.post("/create-task", handleCreateTask)
 routes.get("/get-task", handleGetAllTask)
-//router.put("/:id", updateTask)
-//router.delete("/:id", deleteTask)
-//
+routes.put("/put-task/:id", handleUpdateTask)
+routes.delete("/task/:id", handleDeleteTask )
+
 export {routes}
