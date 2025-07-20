472c77fb7f61732765701bc73e0fc4e6e3e381cb
Viviane Silva <viviane.silva1810@gmail.com>
2025-07-20 19:44:55 -0300

    chore: ajuste de commit

--- src/controllers/put_task.controller.ts
+++ src/controllers/put_task.controller.ts
@@ -5,30 +5,35 @@ import { z } from "zod";
 
 async function handleUpdateTask(request: Request, response: Response) {
 
-    const schemaTask = z.object({
-        id: z.number().min(1, "ID must be a positive number"),
-        title: z.string().min(1, "Title is required"),
-    });
+    try {
+        const schemaTask = z.object({
+            id: z.number().min(1, "ID must be a positive number"),
+            title: z.string().min(1, "Title is required"),
+        });
 
-    const result = schemaTask.safeParse(request.body);
-if (!result.success) {
-    return response.status(400).json({
-        message: "Parametros inválidos",
-        errors: result.error
-    });
-} 
+        const result = schemaTask.safeParse(request.body);
+        if (!result.success) {
+            return response.status(400).json({
+                message: "Parametros inválidos",
+                errors: result.error
+            });
+        }
 
-const { id, title } = result.data 
+        const { id, title } = result.data
 
-    const updateTask = await prisma.task.update({
-        where: { id: Number(id) },
-        data: {
-            title,
-            updateAt: new Date(),
-        },
-    })
+        const updateTask = await prisma.task.update({
+            where: { id: Number(id) },
+            data: {
+                title,
+                updateAt: new Date(),
+            },
+        })
 
-    return response.send(updateTask)
+        return response.send(updateTask)
+
+    } catch (error) {
+        return response.status(404).json({ message: "Task não encontrada", error });
+    }
 }
 
 
